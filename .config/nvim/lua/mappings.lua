1109f35eb45a55494f4d6e2edb796d9b50e53be6
403f2e <shadowouss1@gmail.com>
2025-09-01 18:03:05 +0100

    feat: (nvim) add moving selected lines.

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -24,8 +24,8 @@ map("i", "jk", "<ESC>")
 map("n", "<C-Up>", ":move .-2<CR>==", { desc = "Move line up" })
 map("n", "<C-Down>", ":move .+1<CR>==", { desc = "Move line down" })
 
-map("v", "<C-Up>", ":move '<,'>-2<CR>gv=gv", { desc = "Move line up" })
-map("v", "<C-Down>", ":move '<,'>+1<CR>gv=gv", { desc = "Move line down" })
+map("v", "<C-Up>", ":move '<-2<CR>gv=gv", { desc = "Move line up" })
+map("v", "<C-Down>", ":move '>+1<CR>gv=gv", { desc = "Move line down" })
 
 map("n", "<leader>dn", vim.diagnostic.goto_next, { desc = "Go to Next Error" })
 
